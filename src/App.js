a97b32d7c9a54cec36fa524d35f2c8f5ebaa849d
Sulaeman <sulaeman@rasyid.com>
2019-10-19 12:30:20 +0700

    Tes Aplikasi

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import Container from './component/ScreenContainer';
-class App extends Component {
-  render() {
-    return <Container />;
-  }
-}
-
-export default App;
---
+++ src/Index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Container from './component/ScreenContainer';
+import {StyleSheet, Text, View, Button} from 'react-native';
+class Index extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Haiii iya kassmu</Text>
+      </View>
+    );
+  }
+}
+
+export default Index;
