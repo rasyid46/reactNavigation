f318356b2b45d0412ed0b894a21cb45b7acd0b8f
figofosandy <figofosandy@gmail.com>
2019-10-21 10:36:43 +0700

    Week 5 Day 1

--- android/app/src/main/java/com/reactnativenav/MainActivity.java
+++ android/app/src/main/java/com/reactnativenav/MainActivity.java
@@ -4,6 +4,11 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
   /**
