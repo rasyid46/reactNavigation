389d81511e659984032cfa6e1b6fbb3dd3645ba9
abdul aziz <ajitagustiani10@gmail.com>
2019-10-21 14:11:16 +0700

    repair 1

--- src/raka/HomeActivity.js
+++ src/raka/HomeActivity.js
@@ -1,42 +1,43 @@
 import React, {Component} from 'react';
-import{View,StyleSheet,Text,Button}from 'react-native';
+import  {View, StyleSheet, Text, Button}  from 'react-native';
 
-    class HomeActivity extends Component{
-        static navigationOptions={
-            title:'Home',
-            headerStyle:{
-                backg: '#03A9F4'
-            },
-            headerTintColor:'#fff',
-            headerTitleStyle:{
-                fontWeight:'bold',
-            },
-        };
-        render(){
-            return(
-                <View style={styles.container}>
-                    <Text style={styles.headerText}>Home Activity</Text>
-                    <Button 
-                        title="Go Profile Activity"
-                        onPress={()=> this.props.navigation.navigate('Profile')}/>
-                </View>
-            );
-        }
-    }
+class HomeActivity extends Component {
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#03A9F4',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+  render() {
+    return  (
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Home Activity</Text>
+        <Button
+          title="Go Profile Activity"
+          onPress={() => this.props.navigation.navigate('Profile')}
+        />
+      </View>
+    );
+  }
+}
 
-    const styles=StyleSheet.create({
-        container:{
-            flex:1,
-            justifyContent:'center',
-            alignItems:'center',
-            backgroundColor:'#F5FCFF'
-        },
-        headerText:{
-            fontSize:20,
-            textAlign:'center',
-            margin:10,
-            fontWeight: 'bold'
-        },
-    });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: 'bold'
+  },
+});
 
-    export default HomeActivity;
+export default HomeActivity;
