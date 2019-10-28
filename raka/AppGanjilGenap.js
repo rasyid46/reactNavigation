999bbf06c7c87fad94db6c30eebc1e4faf6d46f8
abdul aziz <ajitagustiani10@gmail.com>
2019-10-28 15:41:42 +0700

    complete

--- raka/AppGanjilGenap.js
+++ raka/AppGanjilGenap.js
@@ -1,14 +1,65 @@
 import React, {Component} from 'react'; //1
-import {StyleSheet, View, Text, TextInput, Button} from 'react-native'; //1
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  FlatList,
+} from 'react-native'; //1
 //17 import component redux
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux'; //connect react component ke redux store
 import {actionGanjilGenap} from './actions/Index';
 //1
 class AppGanjilGenap extends Component {
+  //24
+  state = {
+    inputNumber: '',
+  };
+
+  //25
+  numberChangeHandler = value => {
+    this.setState({
+      inputNumber: value,
+    });
+  };
+
+  //29
+  numberOutput = () => {
+    return (
+      <FlatList
+        data={this.props.listNumberFormReducers}
+        keyExtractor={(item, indeks) => indeks.toString()}
+        renderItem={info => <Text>{info.item.value}</Text>}
+      />
+    );
+  };
+
   prosesSubmit = () => {
+    //30
+    let pesan = '';
+    //28
+    let inputanUser = this.state.inputNumber;
+    //31
+    if (inputanUser == 0) {
+      pesan = 'bilangan 0';
+    } else if (inputanUser % 2 === 0) {
+      pesan = inputanUser + ' bilangan genap';
+    } else if (inputanUser % 2 === 1) {
+      pesan = inputanUser + ' bilangan ganjil';
+    } else {
+      pesan = inputanUser + ' bukan angka';
+    }
     //3
     alert('yosh');
+    //21 panggil
+    this.props.actionGanjilGenap(pesan);
+    //22
+    // console.log(this.props.listNumberFormReducers);
+    //23
+    let dataInputan = this.props.listNumberFormReducers;
+    console.log(dataInputan);
   };
 
   //1
@@ -20,12 +71,20 @@ class AppGanjilGenap extends Component {
     //-yarn add react redux
     //-yarn start
     //5 pindah ke folder Actions
+    //26 onChangeText={this.numberChangeHandler}
+    //27 value={this.state.inputNumber}
+    //31 this.numberOutput()
 
     return (
       <View>
         <Text>Aplikasi Ganjil Genap</Text>
-        <TextInput placeholder="input angka" />
+        <TextInput
+          placeholder="input angka"
+          onChangeText={this.numberChangeHandler}
+          value={this.state.inputNumber}
+        />
         <Button title="proses" onPress={this.prosesSubmit} />
+        {this.numberOutput()}
       </View>
     );
   }
@@ -37,7 +96,7 @@ function mapStateToProps(state) {
   console.log('mengambil data dari store reducers');
   console.log(state);
   return {
-    listNumberFormReducers: state,
+    listNumberFormReducers: state.reducerGanjilGenap.listInputan,
   };
 }
 //19
